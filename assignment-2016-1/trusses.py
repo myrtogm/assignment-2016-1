560ef202424f693f8bdd7cf820d8b92f7fedea41
myrtogm <myrto.gm@gmail.com>
2016-03-25 18:38:39 +0200

    trusses.py

---
+++ assignment-2016-1/trusses.py
@@ -0,0 +1,27 @@
+
+g=[]
+g = open(input("please enter a file"),'r')
+edge = g.readline(a,b)
+for line=() in g:
+   g.append(line.strip())
+
+k = int(input("Please enter the size of truss"))
+adjList = []
+def find_k_truss(g,k):
+   for edge=(a,b) in g:
+       if  (size(intersection(neighbours(a), neighbours(b)) < k - 2:
+               g.remove(edge)
+               return g
+       elif adjlist.append(edge))
+         print("Print all adjacency lists with corresponding vertex")
+            n = len(adjLists)
+            for v in range(0,n):
+                print(v, ":", adjLists[v])
+
+
+
+
+
+
+
+
